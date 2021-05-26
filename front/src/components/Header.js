728785a363af5bf0a501f2744d4c68c3fb9ceb82
Erick Vicentin <erickvicentin16@gmail.com>
2021-05-26 19:58:44 -0300

    Code Improvements

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -1,50 +1,50 @@
-import React from 'react';
+import React from "react";
 import { Link } from "react-router-dom";
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import logo from '../assets/logo.png'
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import logo from "../assets/logo.png";
 
 const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
 }));
 
 export default function Header() {
-    const classes = useStyles();
+  const classes = useStyles();
 
-    return (
-      <div style={{background: '#ffA500'}} className={classes.root}>
-        <AppBar position="static" color='transparent'>
-          <Toolbar>
-            {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+  return (
+    <div style={{ background: "#ffA500" }} className={classes.root}>
+      <AppBar position="static" color="transparent">
+        <Toolbar>
+          {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton> */}
-            <Typography variant="h6" className={classes.title}>
-              <Link to="/" style={{ textDecoration: "none" }}>
-                            <img src={logo} alt="Logo" style={{height: 35}}/>
-              </Link>
-            </Typography>
-            <Link to="/about-us" style={{ textDecoration: "none" }}>
-              <Button color="default">About</Button>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/" style={{ textDecoration: "none" }}>
+              <img src={logo} alt="Logo" style={{ height: 35 }} />
             </Link>
-            <Link to="/contact" style={{ textDecoration: "none" }}>
-              <Button color="default">Contact</Button>
-            </Link>
-            <Link to="/profile" style={{ textDecoration: "none" }}>
-              <Button color="default">Profile</Button>
-            </Link>
-          </Toolbar>
-        </AppBar>
-      </div>
-    );
+          </Typography>
+          <Link to="/about-us" style={{ textDecoration: "none" }}>
+            <Button color="default">About</Button>
+          </Link>
+          <Link to="/contact" style={{ textDecoration: "none" }}>
+            <Button color="default">Contact</Button>
+          </Link>
+          <Link to="/profile" style={{ textDecoration: "none" }}>
+            <Button color="default">Profile</Button>
+          </Link>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
