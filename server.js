1ebbf2f82b132cc044153b5658f15443e87749d0
JoseCabreraJC <josem.cabrera259@gmail.com>
2021-05-26 09:18:41 -0300

    added mongodb server connection

--- server.js
+++ server.js
@@ -1,7 +1,29 @@
 const express = require("express");
-const secretaryRoutes = require("./server/routes/index");
-const app = express();
 const port = 3000;
+
+const secretaryRoutes = require("./server/routes/index");
+
+const app = express();
+
+const { url } = require("./server/config/db.config.js");
+const mongoose = require("mongoose");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to the database!");
+  })
+  .catch(err => {
+    console.log("Cannot connect to the database!", err);
+    process.exit();
+  });
 
 app.use("/", secretaryRoutes);
 
