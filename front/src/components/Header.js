f22c8b82088a3d6a5b8a8812293c210de8485daf
Erick Vicentin <erickvicentin14@hotmail.com>
2021-05-26 19:55:41 -0300

    [Feat] Added logo & header color

--- front/src/components/Header.js
+++ front/src/components/Header.js
@@ -5,6 +5,7 @@ import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import Button from '@material-ui/core/Button';
+import logo from '../assets/logo.png'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -22,20 +23,28 @@ export default function Header() {
     const classes = useStyles();
 
     return (
-        <div className={classes.root}>
-            <AppBar position="static" color="primary">
-                <Toolbar>
-                    {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
+      <div style={{background: '#ffA500'}} className={classes.root}>
+        <AppBar position="static" color='transparent'>
+          <Toolbar>
+            {/* <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
                         <MenuIcon />
                     </IconButton> */}
-                    <Typography variant="h6" className={classes.title}>
-                        <Link to="/" style={{ textDecoration: 'none' }}>Los cracks</Link>
-                    </Typography>
-                    <Link to="/about-us" style={{ textDecoration: 'none' }} ><Button color="inherit">About</Button></Link>
-                    <Link to="/contact" style={{ textDecoration: 'none' }} ><Button color="inherit">Contact</Button></Link>
-                    <Link to="/profile" style={{ textDecoration: 'none' }} ><Button color="inherit">Profile</Button></Link>
-                </Toolbar>
-            </AppBar>
-        </div>
+            <Typography variant="h6" className={classes.title}>
+              <Link to="/" style={{ textDecoration: "none" }}>
+                            <img src={logo} alt="Logo" style={{height: 35}}/>
+              </Link>
+            </Typography>
+            <Link to="/about-us" style={{ textDecoration: "none" }}>
+              <Button color="default">About</Button>
+            </Link>
+            <Link to="/contact" style={{ textDecoration: "none" }}>
+              <Button color="default">Contact</Button>
+            </Link>
+            <Link to="/profile" style={{ textDecoration: "none" }}>
+              <Button color="default">Profile</Button>
+            </Link>
+          </Toolbar>
+        </AppBar>
+      </div>
     );
 }
--- front/src/layouts/LayoutBasic.js
+++ front/src/layouts/LayoutBasic.js
@@ -1,19 +1,21 @@
-import React from "react"
+import React from "react";
 
-import LoadRoutes from "config/LoadRoutes"
+import LoadRoutes from "config/LoadRoutes";
 
-import Header from "components/Header"
-import Footer from "components/Footer"
-
+import Header from "components/Header";
+import Footer from "components/Footer";
 
 export default function LayoutBasic(props) {
-    const { routes } = props
+  const { routes } = props;
 
-    return (
-        <div>
-            <Header />
-            <LoadRoutes routes={routes} />
-            <Footer title="Los cracks" description="Página diseñada para la matería de DACS" />
-        </div>
-    )
+  return (
+    <>
+      <Header />
+      <LoadRoutes routes={routes} />
+      <Footer
+        title="Los cracks"
+        description="Página diseñada para la matería de DACS"
+      />
+    </>
+  );
 }
