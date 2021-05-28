1be92b140e29b2cb6f560ce96ea30cdcbff41ff7
jorchohyeah <jorgecurima.utn@gmail.com>
2021-05-28 17:31:07 -0300

    Added environment variable port

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
-const port = 8081;
+const port = process.env.PORT;
 
 const libreria = require("dacs-integrador-g5");
 
