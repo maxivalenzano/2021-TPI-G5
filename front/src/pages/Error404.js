126e25b06287f2f115dcbe9b8ec1e5c6d78af7cb
Erick Vicentin <erickvicentin16@gmail.com>
2021-05-27 22:30:32 -0300

    Style Added to 404 page

--- front/src/pages/Error404.js
+++ front/src/pages/Error404.js
@@ -1,11 +1,27 @@
 import React from 'react'
+import errorImage from '../assets/error.png'
 
 export default function Erro404() {
     return (
-        <div style={{backgroundColor: "red", display:'flex', alignItems:"center", justifyContent: "center", justifyItems:"center"}}>
-            <h2>
-                Error 404...
-            </h2>
+        <div style={styles.container}>
+            <img src={errorImage} alt='404 Error' style={{height: 300}}/>
+            <text style={styles.text}>Error! This page not found.</text>
         </div>
     )
 }
+
+const styles = {
+  text: {
+    fontSize: 24,
+    fontFamily: "monospace",
+    paddingBottom: 20
+  },
+  container: {
+    backgroundColor: "white",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+    justifyItems: "center",
+    flexDirection: "column",
+  },
+};
