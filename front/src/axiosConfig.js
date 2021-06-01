23bfee42c9d973ebeaea79379b5d9b76ba1dafe5
Maximiliano <maxivalenzano@gmail.com>
2021-06-01 16:15:28 -0300

    fix axios

--- front/src/axiosConfig.js
+++ front/src/axiosConfig.js
@@ -3,7 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://secure-sands-97755.herokuapp.com",
   headers: {
-    "Content-type": "application/json",
-    Authorization: "Bearer tokenardo",
+    "Content-type": "application/json"
   },
 });
