0e14fa0532634c7d1a1262dbdab2d3d81cc53821
Maximiliano <maxivalenzano@gmail.com>
2021-07-16 11:43:27 -0300

    commit de deploy

--- front/src/App.js
+++ front/src/App.js
@@ -1,30 +1,25 @@
-import React from "react"
-import { BrowserRouter, Switch } from 'react-router-dom';
-import routes from 'config/routes';
-import authRoutes from 'config/auth.routes';
-import PrivateRouterWithSubRoutes from 'components/PrivateRouters'
-import RouterWithSubRoutes from 'components/RouterWithSubRoutes'
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+import routes from "config/routes";
+import authRoutes from "config/auth.routes";
+import PrivateRouterWithSubRoutes from "components/PrivateRouters";
+import RouterWithSubRoutes from "components/RouterWithSubRoutes";
 
 const App = () => {
-
   // console.log("Cookies: ", auth)
   return (
     <BrowserRouter>
       <Switch>
-        {
-          authRoutes.map((route, index) => (
-            <RouterWithSubRoutes key={index} {...route} />
-          ))
-        }
+        {authRoutes.map((route, index) => (
+          <RouterWithSubRoutes key={index} {...route} />
+        ))}
 
-        {
-          routes.map((route, index) => (
-            <PrivateRouterWithSubRoutes key={index} {...route} />
-          ))
-        }
+        {routes.map((route, index) => (
+          <PrivateRouterWithSubRoutes key={index} {...route} />
+        ))}
       </Switch>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
